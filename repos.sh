8502d176db8645e677ede44d17a58dad63eb759a
Wiliam Souza <wiliamsouza83@gmail.com>
2014-05-23 22:27:47 -0300

    Added github remote

--- repos.sh
+++ repos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git remote add github git@github.com:wiliamsouza/apollo-agent.git
 git remote add bitbucket git@bitbucket.org:wiliamsouza/apollo-agent.git
 git remote add gitlab git@gitlab.com:wiliamsouza/apollo-agent.git
 git remote set-url --push --add origin git@bitbucket.org:wiliamsouza/apollo-agent.git
