cbd7b91cb34ad2b1551e997c6c0af48647b44308
Wiliam Souza <wiliamsouza83@gmail.com>
2014-05-23 22:18:45 -0300

    Added script to set up git mirror repositories

---
+++ repos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git remote add bitbucket git@bitbucket.org:wiliamsouza/apollo-agent.git
+git remote add gitlab git@gitlab.com:wiliamsouza/apollo-agent.git
+git remote set-url --push --add origin git@bitbucket.org:wiliamsouza/apollo-agent.git
+git remote set-url --push --add origin git@gitlab.com:wiliamsouza/apollo-agent.git
+git remote set-url --push --add origin git@github.com:wiliamsouza/apollo-agent.git
